3a3ab78776ad6dbd91a91ae5e4758e6bb7c06568
agilalatas <agilalatas17@gmail.com>
2024-12-13 14:09:59 +0700

    feat/POST-01/create migration, model dan seeder untuk posts db

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $judul = [
+            'Artikel Satu',
+            'Artikel Dua',
+            'Artikel Tiga',
+            'Artikel Empat',
+            'Artikel Lima'
+        ];
+
+        foreach($judul as $j) {
+            $slug = Str::slug($j);
+            $slugOri = $slug;
+            $count = 1;
+
+            while(Post::where('slug', $slug)->exists()){
+                $slug = $slugOri . '-' . $count;
+                $count++;
+            }
+
+            Post::create([
+                'judul' => $j,
+                'slug' => $slug,
+                'deskripsi' => 'Deskripsi untuk judul ' . $j,
+                'konten' => 'Konten untuk ' . $j,
+                'tipe' => 'artikel',
+                'status' => 'publish'
+            ]);
+        }
+    }
+}
