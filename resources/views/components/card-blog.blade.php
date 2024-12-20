4bd64b14d65ad21164b7761126b27e5ebc870032
agilalatas <agilalatas17@gmail.com>
2024-12-21 00:18:41 +0700

    fix/benerin UI card component

--- resources/views/components/card-blog.blade.php
+++ resources/views/components/card-blog.blade.php
@@ -1,26 +1,28 @@
 @props(['title', 'content', 'category', 'date', 'thumbnail', 'link'])
 
-<div class="max-w-xs overflow-hidden bg-white rounded-lg shadow-md">
-    @if (isset($thumbnail))
-        <img class="object-cover object-center w-full h-60 rounded-lg lg:h-64" src="{{ $thumbnail }}" alt="Artikel">
-    @else
-        <img class="object-cover object-center w-full h-60 rounded-lg lg:h-64"
-            src="https://t4.ftcdn.net/jpg/05/17/53/57/360_F_517535712_q7f9QC9X6TQxWi6xYZZbMmw5cnLMr279.jpg"
-            alt="gambar artikel">
-    @endif
+<div class="overflow-hidden bg-white rounded-lg shadow-md">
+    <a href="{{ $link }}">
+        @if ($thumbnail)
+            <img class="object-contain object-center w-full h-60 rounded-lg lg:h-64"
+                src="{{ asset(getenv('THUMBNAILS_LOCATION') . '/' . $thumbnail) }}" alt="Artikel image">
+        @else
+            <img class="object-cover object-center w-full h-60 rounded-lg lg:h-64"
+                src="{{ asset('assets/images/general/default-image.jpg') }}">
+        @endif
+    </a>
 
     <div class="px-6 pt-4 pb-6">
         <div>
             <div class="flex justify-between items-center">
                 <span
                     class="d-block text-xs font-medium bg-pink-500 px-2 py-1 rounded text-white uppercase">{{ Str::upper($category) }}</span><span
-                    class="d-block text-sm font-medium text-pink-500">{{ $date->diffForHumans() }}</span>
+                    class="d-block text-sm font-medium text-pink-500">{{ $date }}</span>
             </div>
             <a href="{{ $link }}"
                 class="block mt-4 text-xl font-semibold text-gray-800 transition-colors duration-300 transform hover:text-purple-500 hover:underline"
                 tabindex="0" role="link">{{ Str::title($title) }}</a>
             <p class="mt-2 text-base text-gray-600">
-                {!! Str::limit(strip_tags($content), 100, '...') !!}
+                {!! Str::limit($content, 100, '...') !!}
 
             </p>
         </div>
