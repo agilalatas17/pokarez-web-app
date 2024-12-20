f67629cb3e1947b3350034ca722000a3a903195d
agilalatas <agilalatas17@gmail.com>
2024-12-21 00:19:53 +0700

    feat/membuat query untuk menampilkan data blog

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index() {
-        return view('articles-page');
+        $data = Post::where('status', 'publish')->where('kategori', 'artikel')->orderBy('created_at', 'desc')->get();
+        
+        return view('articles-page', compact('data'));
     }
 }
