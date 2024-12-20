e8b355222b788be30c77217d2472e6ea0cefea80
agilalatas <agilalatas17@gmail.com>
2024-12-21 00:21:13 +0700

    feat/membuat query untuk menampilkan detail blog dan benerin styling halaman blog-detail

--- resources/views/blog-detail-page.blade.php
+++ resources/views/blog-detail-page.blade.php
@@ -1,205 +1,28 @@
 <x-layout>
     <x-slot name="header">
-        <h1 class="font-bold text-base md:text-2xl lg:text-3xl text-left text-gray-800">
-            BENAR ‘KAH SARAPAN DAPAT MEMPENGARUHI PRESTASI BELAJAR PADA REMAJA?
+        <h1 class="font-bold text-2xl md:text-3xl lg:text-4xl text-left text-gray-800">
+            {{ $data->judul }}
         </h1>
-        <p class="text-xs md:text-sm lg:text-base mt-4">Diposting pada 17 Desember 2024 oleh Indri Ardelia</p>
+        <p class="text-xs md:text-sm lg:text-base mt-4">Diposting pada
+            <span>{{ $data->created_at->isoFormat('dddd, DD MMMM Y') }}</span> oleh
+            <span>{{ Str::title($data->user->name) }}</span>
+        </p>
     </x-slot>
 
-    <div class="container px-8 py-8 mx-auto">
+    <div class="container px-4 md:px-8 md:py-8 mx-auto">
         <article>
-            {{-- @isset($thumbnail)
-            <img src="https://via.placeholder.com/1024x512" alt="">
-        @endisset --}}
-
             <div class="px-2 mb-20">
-                <img src="https://via.placeholder.com/1024x512"
-                    class="object-cover object-center max-w-[5xl] max-h-[512px] mx-auto">
+                @if ($data->thumbnail)
+                    <img src="{{ asset(getenv('THUMBNAILS_LOCATION') . '/' . $data->thumbnail) }}"
+                        class="object-cover object-center max-w-[5xl] max-h-[512px] mx-auto">
+                @else
+                    <img src="{{ asset('assets/images/general/default-image.jpg') }}"
+                        class="object-cover object-center max-w-[5xl] max-h-[512px] mx-auto">
+                @endif
             </div>
 
             <div>
-                <h3>Ini sub judul gaesss</h3>
-                <p>
-                    TANGERANG, KOMPAS.com - Terpidana mati kasus narkoba, Mary Jane Veloso menyanyikan lagu Indonesia
-                    Raya
-                    sebelum dipulangkan ke negara asalnya, Filipina. Momen itu terjadi saat Mary Jane hadir dalam
-                    konferensi
-                    pers rencana kepulangannya di Terminal 2F Bandara Soekarno Hatta, Kota Tangerang, Selasa
-                    (17/12/2024).
-                    "Kami
-                    punya sesuatu untuk kalian," ujar Wakil Menteri Urusan Migrasi, Departemen Luar Negeri Filipina,
-                    Eduardo
-                    Jose De Vega dalam bahasa Inggris di hadapan awak media. Kemudian, Eduardo langsung memberikan
-                    aba-aba
-                    untuk
-                    menyanyikan lagu kebangsaan Indonesia bersama Mary Jane.
-                </p>
-                <p>
-                    Artikel ini telah tayang di Kompas.com dengan judul "Momen Mary Jane Nyanyikan Lagu Indonesia Raya
-                    Sebelum
-                    Pulang ke Filipina... ", Klik untuk baca:
-                    https://megapolitan.kompas.com/read/2024/12/17/22551821/momen-mary-jane-nyanyikan-lagu-indonesia-raya-sebelum-pulang-ke-filipina.
-                </p>
-                <p>
-                    Kompascom+ baca berita tanpa iklan: https://kmp.im/plus6
-                    Download aplikasi: https://kmp.im/app6
-                </p>
-                <h3>Ini sub judul gaesss</h3>
-                <p>
-                    TANGERANG, KOMPAS.com - Terpidana mati kasus narkoba, Mary Jane Veloso menyanyikan lagu Indonesia
-                    Raya
-                    sebelum dipulangkan ke negara asalnya, Filipina. Momen itu terjadi saat Mary Jane hadir dalam
-                    konferensi
-                    pers rencana kepulangannya di Terminal 2F Bandara Soekarno Hatta, Kota Tangerang, Selasa
-                    (17/12/2024).
-                    "Kami
-                    punya sesuatu untuk kalian," ujar Wakil Menteri Urusan Migrasi, Departemen Luar Negeri Filipina,
-                    Eduardo
-                    Jose De Vega dalam bahasa Inggris di hadapan awak media. Kemudian, Eduardo langsung memberikan
-                    aba-aba
-                    untuk
-                    menyanyikan lagu kebangsaan Indonesia bersama Mary Jane.
-                </p>
-                <p>
-                    Artikel ini telah tayang di Kompas.com dengan judul "Momen Mary Jane Nyanyikan Lagu Indonesia Raya
-                    Sebelum
-                    Pulang ke Filipina... ", Klik untuk baca:
-                    https://megapolitan.kompas.com/read/2024/12/17/22551821/momen-mary-jane-nyanyikan-lagu-indonesia-raya-sebelum-pulang-ke-filipina.
-                </p>
-                <p>
-                    Kompascom+ baca berita tanpa iklan: https://kmp.im/plus6
-                    Download aplikasi: https://kmp.im/app6
-                </p>
-                <h3>Ini sub judul gaesss</h3>
-                <p>
-                    TANGERANG, KOMPAS.com - Terpidana mati kasus narkoba, Mary Jane Veloso menyanyikan lagu Indonesia
-                    Raya
-                    sebelum dipulangkan ke negara asalnya, Filipina. Momen itu terjadi saat Mary Jane hadir dalam
-                    konferensi
-                    pers rencana kepulangannya di Terminal 2F Bandara Soekarno Hatta, Kota Tangerang, Selasa
-                    (17/12/2024).
-                    "Kami
-                    punya sesuatu untuk kalian," ujar Wakil Menteri Urusan Migrasi, Departemen Luar Negeri Filipina,
-                    Eduardo
-                    Jose De Vega dalam bahasa Inggris di hadapan awak media. Kemudian, Eduardo langsung memberikan
-                    aba-aba
-                    untuk
-                    menyanyikan lagu kebangsaan Indonesia bersama Mary Jane.
-                </p>
-                <p>
-                    Artikel ini telah tayang di Kompas.com dengan judul "Momen Mary Jane Nyanyikan Lagu Indonesia Raya
-                    Sebelum
-                    Pulang ke Filipina... ", Klik untuk baca:
-                    https://megapolitan.kompas.com/read/2024/12/17/22551821/momen-mary-jane-nyanyikan-lagu-indonesia-raya-sebelum-pulang-ke-filipina.
-                </p>
-                <p>
-                    Kompascom+ baca berita tanpa iklan: https://kmp.im/plus6
-                    Download aplikasi: https://kmp.im/app6
-                </p>
-                <h3>Ini sub judul gaesss</h3>
-                <p>
-                    TANGERANG, KOMPAS.com - Terpidana mati kasus narkoba, Mary Jane Veloso menyanyikan lagu Indonesia
-                    Raya
-                    sebelum dipulangkan ke negara asalnya, Filipina. Momen itu terjadi saat Mary Jane hadir dalam
-                    konferensi
-                    pers rencana kepulangannya di Terminal 2F Bandara Soekarno Hatta, Kota Tangerang, Selasa
-                    (17/12/2024).
-                    "Kami
-                    punya sesuatu untuk kalian," ujar Wakil Menteri Urusan Migrasi, Departemen Luar Negeri Filipina,
-                    Eduardo
-                    Jose De Vega dalam bahasa Inggris di hadapan awak media. Kemudian, Eduardo langsung memberikan
-                    aba-aba
-                    untuk
-                    menyanyikan lagu kebangsaan Indonesia bersama Mary Jane.
-                </p>
-                <p>
-                    Artikel ini telah tayang di Kompas.com dengan judul "Momen Mary Jane Nyanyikan Lagu Indonesia Raya
-                    Sebelum
-                    Pulang ke Filipina... ", Klik untuk baca:
-                    https://megapolitan.kompas.com/read/2024/12/17/22551821/momen-mary-jane-nyanyikan-lagu-indonesia-raya-sebelum-pulang-ke-filipina.
-                </p>
-                <p>
-                    Kompascom+ baca berita tanpa iklan: https://kmp.im/plus6
-                    Download aplikasi: https://kmp.im/app6
-                </p>
-                <h3>Ini sub judul gaesss</h3>
-                <p>
-                    TANGERANG, KOMPAS.com - Terpidana mati kasus narkoba, Mary Jane Veloso menyanyikan lagu Indonesia
-                    Raya
-                    sebelum dipulangkan ke negara asalnya, Filipina. Momen itu terjadi saat Mary Jane hadir dalam
-                    konferensi
-                    pers rencana kepulangannya di Terminal 2F Bandara Soekarno Hatta, Kota Tangerang, Selasa
-                    (17/12/2024).
-                    "Kami
-                    punya sesuatu untuk kalian," ujar Wakil Menteri Urusan Migrasi, Departemen Luar Negeri Filipina,
-                    Eduardo
-                    Jose De Vega dalam bahasa Inggris di hadapan awak media. Kemudian, Eduardo langsung memberikan
-                    aba-aba
-                    untuk
-                    menyanyikan lagu kebangsaan Indonesia bersama Mary Jane.
-                </p>
-                <p>
-                    Artikel ini telah tayang di Kompas.com dengan judul "Momen Mary Jane Nyanyikan Lagu Indonesia Raya
-                    Sebelum
-                    Pulang ke Filipina... ", Klik untuk baca:
-                    https://megapolitan.kompas.com/read/2024/12/17/22551821/momen-mary-jane-nyanyikan-lagu-indonesia-raya-sebelum-pulang-ke-filipina.
-                </p>
-                <p>
-                    Kompascom+ baca berita tanpa iklan: https://kmp.im/plus6
-                    Download aplikasi: https://kmp.im/app6
-                </p>
-                <h3>Ini sub judul gaesss</h3>
-                <p>
-                    TANGERANG, KOMPAS.com - Terpidana mati kasus narkoba, Mary Jane Veloso menyanyikan lagu Indonesia
-                    Raya
-                    sebelum dipulangkan ke negara asalnya, Filipina. Momen itu terjadi saat Mary Jane hadir dalam
-                    konferensi
-                    pers rencana kepulangannya di Terminal 2F Bandara Soekarno Hatta, Kota Tangerang, Selasa
-                    (17/12/2024).
-                    "Kami
-                    punya sesuatu untuk kalian," ujar Wakil Menteri Urusan Migrasi, Departemen Luar Negeri Filipina,
-                    Eduardo
-                    Jose De Vega dalam bahasa Inggris di hadapan awak media. Kemudian, Eduardo langsung memberikan
-                    aba-aba
-                    untuk
-                    menyanyikan lagu kebangsaan Indonesia bersama Mary Jane.
-                </p>
-                <p>
-                    Artikel ini telah tayang di Kompas.com dengan judul "Momen Mary Jane Nyanyikan Lagu Indonesia Raya
-                    Sebelum
-                    Pulang ke Filipina... ", Klik untuk baca:
-                    https://megapolitan.kompas.com/read/2024/12/17/22551821/momen-mary-jane-nyanyikan-lagu-indonesia-raya-sebelum-pulang-ke-filipina.
-                </p>
-                <p>
-                    Kompascom+ baca berita tanpa iklan: https://kmp.im/plus6
-                    Download aplikasi: https://kmp.im/app6
-                </p>
-                <h3>Ini sub judul gaesss</h3>
-                <p>
-                    TANGERANG, KOMPAS.com - Terpidana mati kasus narkoba, Mary Jane Veloso menyanyikan lagu Indonesia
-                    Raya
-                    sebelum dipulangkan ke negara asalnya, Filipina. Momen itu terjadi saat Mary Jane hadir dalam
-                    konferensi
-                    pers rencana kepulangannya di Terminal 2F Bandara Soekarno Hatta, Kota Tangerang, Selasa
-                    (17/12/2024).
-                    "Kami
-                    punya sesuatu untuk kalian," ujar Wakil Menteri Urusan Migrasi, Departemen Luar Negeri Filipina,
-                    Eduardo
-                    Jose De Vega dalam bahasa Inggris di hadapan awak media. Kemudian, Eduardo langsung memberikan
-                    aba-aba
-                    untuk
-                    menyanyikan lagu kebangsaan Indonesia bersama Mary Jane.
-                </p>
-                <p>
-                    Artikel ini telah tayang di Kompas.com dengan judul "Momen Mary Jane Nyanyikan Lagu Indonesia Raya
-                    Sebelum
-                    Pulang ke Filipina... ", Klik untuk baca:
-                    https://megapolitan.kompas.com/read/2024/12/17/22551821/momen-mary-jane-nyanyikan-lagu-indonesia-raya-sebelum-pulang-ke-filipina.
-                </p>
-                <p>
-                    Kompascom+ baca berita tanpa iklan: https://kmp.im/plus6
-                    Download aplikasi: https://kmp.im/app6
-                </p>
+                {!! $data->konten !!}
             </div>
         </article>
 
