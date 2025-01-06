63f2d662ec13233e58ae485f86da83aebdad18fd
agilalatas <agilalatas17@gmail.com>
2025-01-06 17:46:03 +0700

    feat/konfigurasi OAuth Youtube API

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\BlogController;
 use App\Http\Controllers\BlogDetailController;
 use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\YoutubeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,6 +29,9 @@ Route::get('/konsultasi', function(){
 Route::get('/tentang-pokarez', function(){
     abort(404);
 })->name('tentang-pokarez');
+
+Route::get('/dashboard/youtube/auth/google', [YoutubeController::class, 'authenticate']);
+Route::get('/dashboard/youtube/callback', [YoutubeController::class, 'callback']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('dashboard', BlogController::class)
